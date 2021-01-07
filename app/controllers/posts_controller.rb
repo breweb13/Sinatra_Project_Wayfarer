e2a99e4f8ca0e2b233f312642ea0b943f4d1975e
Breanna <breanna@breannawebster.com>
2021-01-07 01:32:44 -0500

    edit erb patch form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,37 +7,41 @@ class PostsController < ApplicationController
     erb :"posts/index"
  end
 
- # gets 1 post
- get '/posts/:id' do 
+ # viewing for user to make a new post
+ get '/posts/new' do
+    #display the new view
+    erb :"posts/new"
+end
+
+  #create a new post
+  post '/posts' do
+    @post = Post.new(params)
+    post.save
+    redirect '/posts'
+ end
+
+  # gets 1 post
+  get '/posts/:id' do 
     @post = Post.find_by_id(params["id"])
     erb :"posts/show"
      #displays show view
  end
 
- # viewing for user to make a new post
- get '/posts/new' do
-     #display the new view
- end
-
- #create a new post
- post '/posts' do
- end
-
- # view the form to edit 1 particular post
- get '/posts/:id/edit' do
+  # view the form to edit 1 particular post
+  get '/posts/:id/edit' do
     @post = Post.find_by_id(params["id"])
+    erb :"posts/edit"
      #displays the edit view (render)
- end
+  end
 
- 
- #Update 1 post based on edit form
- patch '/posts/:id' do
+    #Update 1 post based on edit form
+    patch '/posts/:id' do
     @post = Post.find_by_id(params["id"])
- end
+    end
 
- #Delete 1 post
- delete '/posts/:id' do
+    #Delete 1 post
+    delete '/posts/:id' do
     @post = Post.find_by_id(params["id"])
- end
+    end
 
 end
