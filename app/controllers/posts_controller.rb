f0ae336411a9c93e0f0c36fef6cb908b90fd3621
Breanna <breanna@breannawebster.com>
2021-01-09 00:18:16 -0500

    added if statements and notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,87 @@
 class PostsController < ApplicationController
- 
- #gets all post 
- get '/posts' do
-    #display the index view
-    @posts = Post.all
-    erb :"posts/index"
- end
 
- # viewing for user to make a new post
- get '/posts/new' do
-    #display the new view
-    erb :"posts/new"
-end
 
-  #create a new post
-  post '/posts' do
-    @post = Post.new(params)
-    redirect "/users/#{@post.id}"
- end
+    #BUILDING OUT CRUD
+    #Read - showing all posts
 
-  # gets 1 post
-  get '/posts/:id' do 
-    @post = Post.find_by_id(params["id"])
-    erb :"posts/show"
-     #displays show view
- end
-
-  # view the form to edit 1 particular post
-  get '/posts/:id/edit' do
-    @post = Post.find_by_id(params["id"])
-    erb :"posts/edit"
-     #displays the edit view (render)
-  end
-
-    #Update 1 post based on edit form
-    patch '/posts/:id' do
-    @post = Post.find_by_id(params["id"])
+    get '/posts' do
+        #attach the posts model so we can view all from double
+        @posts = Post.all
+        #render all the posts
+        erb :'posts/index'
     end
 
-    #Delete 1 post
+
+    #Create - render a form for user to create a new post
+     get '/posts/new' do
+        redirect_to_if_not_logged_in
+        erb :'posts/new'
+    end
+
+    post '/posts' do
+        @post = Post.new(params[:posts])
+        #binding.pry
+        if @post.save
+          flash[:message] = "Post created successfully!"
+          redirect "/posts/#{@post.id}"
+        else
+          flash[:error] = "Unable to create post: #{@post.errors.full_messages.to_sentence}"
+          redirect '/posts/new'
+        end
+    end
+
+    # an issue with the way code is ordered. 
+
+    #To display a single post
+    get '/posts/:id' do
+        find_post
+        erb :'/posts/show'
+    end
+
+ 
+    #Update
+    #Edit button that takes us to form
+    #Render form to edit post
+    
+    get '/posts/:id/edit' do
+        redirect_to_if_not_logged_in
+        find_post
+        if authorized_to_edit?(@post)
+        erb :'/posts/edit'
+        else
+        flash[:error] = "You are not authorized to edit this post!"
+        redirect "/posts/#{@post.id}"
+        end
+    end
+   
+    #Use PATCH method here.. 
+    patch '/posts/:id' do 
+        
+        find_post
+        @post.update(location: params[:location], image_url: params[:image_url], journal: params[:journal])
+        redirect "/posts/#{@post.id}"
+    end
+    
+    #Delete
+    #Delete the post - as well as the existing route
     delete '/posts/:id' do
-    @post = Post.find_by_id(params["id"])
+        #gotta find the post we want to Delete
+        find_post
+        if authorized_to_edit?(@post)
+        @post.destroy
+        flash[:message] = "Post deleted successfully!"
+        redirect '/posts'
+        else
+            flash[:error] = "You are not authorized to delete this post!"
+            redirect "/posts/#{@post.id}"
+        end
+    end
+    
+    
+    private
+
+    def find_post
+        @post = Post.find_by_id(params[:id])
     end
     
 end
