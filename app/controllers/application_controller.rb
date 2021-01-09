f5443b8a2df2aab0e79b1aee23af601d5a3333d8
Breanna <breanna@breannawebster.com>
2021-01-09 00:17:37 -0500

    added if statement to post and notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     register Sinatra::Flash
-    set :session_secret, "secret_vibes"
+    set :session_secret, "Secret Session"
   end
 
+  
 
   get "/" do
-    
+    if logged_in?
+      redirect to '/posts'
+    else
     erb :welcome
+    end
   end
 
   helpers do 
@@ -26,18 +30,18 @@ class ApplicationController < Sinatra::Base
       User.find_by_id(session[:user_id])
     end
 
+    def authorized_to_edit?(post)
+      post.user == current_user
+    end
+
     def redirect_to_if_logged_in
-      redirect '/posts' if logged_in?
+      redirect '/projects' if logged_in?
     end
 
     def redirect_to_if_not_logged_in
-      redirect '/users/login' unless logged_in?
+      redirect '/login' unless logged_in?
     end
 
-  end
-
-  def authorized_to_edit?(post)      
-    post.user == current_user    
   end
 
 end
